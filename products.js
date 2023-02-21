db10e3aa2e795c3fc32ecc3c0c29cc354ab5d763
tushar jajodia <32388411+tjaj07@users.noreply.github.com>
2023-02-21 16:39:33 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,141 @@
+
+productSkuMapping = {
+    "Corduroy Blazer": 200240,
+    "Military Jacket": 200241,
+    "Hooded Sweater": 200233,
+    "Sport Jacket": 200244,
+    "Two-button Suit": 201240,
+    "Blue Shirt": 201241,
+    "French Cuff Shirt": 201244,
+    "Tuxedo": 201233,
+    "Scrunched Leg Cargo Pants": 100233,
+    "Front-Zip Ski Jacket": 100244,
+    "Fur-Trim Ski Jacket": 100241,
+    "Tie-Waist Puffer Jacket": 100240,
+    "Black Dress": 101340,
+    "Red Dress": 101341,
+    "Sequin Dress": 101333,
+    "Silk Dress": 101344
+}
+
+
+// products = {
+//     "Corduroy Blazer" : {
+//         productSKU : 200240,
+//         productName: "Corduroy Blazer",
+//         productPrice: 709.99,
+//         productCategory: "Men",
+//         productSubCategory: "ActiveWear"
+//     },
+//     "Military Jacket" : {
+//         productName: "Military Jacket",
+//         prodPrice: 750.00,
+//         productSKU: 200241,
+//         productCategory: "Men",
+// 		productSubCategory: "Activewear"
+//     },
+//     "Hooded Sweater": {
+//         productName: "Hooded Sweater",
+//         prodPrice: 450.00,
+//         productSKU: 200233,
+//         productCategory: "Men",
+// 		productSubCategory: "Activewear"
+//     },
+//     "Sport Jacket" : {
+//         productName: "Sport Jacket",
+//         prodPrice: 240.00,
+//         productSKU: 200244,
+//         productCategory: "Men",
+// 		productSubCategory: "Activewear"
+//     },
+
+
+//     "Two-button Suit": {
+//         productName: "Two-button Suit",
+//         prodPrice: 1850.00,
+//         productSKU: 201240,
+//         productCategory: "Men",
+// 		productSubCategory: "Fine Apparel"
+//     },
+//     "Blue Shirt": {
+//         productName: "Blue Shirt",
+//         prodPrice: 289.99,
+//         productSKU: 201241,
+//         productCategory: "Men",
+// 		productSubCategory: "Fine Apparel"
+//     },
+//     "French Cuff Shirt": {
+//         productName: "French Cuff Shirt",
+//         prodPrice: 309.99,
+//         productSKU: 201244,
+//         productCategory: "Men",
+// 		productSubCategory: "Fine Apparel"
+//     },
+//     "Tuxedo": {
+//         productName: "Tuxedo",
+//         prodPrice: 2600.00,
+//         productSKU: 201233,
+//         productCategory: "Men",
+// 		productSubCategory: "Fine Apparel"
+//     },
+
+//     "Scrunched Leg Cargo Pants": {
+//         productName: "Scrunched Leg Cargo Pants",
+//         prodPrice: 650.00,
+//         productSKU: 100233,
+//         productCategory: "Women",
+// 		productSubCategory: "Activewear",
+//     },
+//     "Front-Zip Ski Jacket": {
+//         productName: "Front-Zip Ski Jacket",
+//         prodPrice: 1714.99,
+//         productSKU: 100244,
+//         productCategory: "Women",
+// 		productSubCategory: "Activewear",
+//     },
+//     "Fur Trim Ski Jacket": {
+//         productName: "Fur Trim Ski Jacket",
+//         prodPrice: 2259.99,
+//         productSKU: 100241,
+//         productCategory: "Women",
+// 		productSubCategory: "Activewear",
+//     },
+//     "Tie-Waist Puffer Jacket": {
+//         productName: "Tie-Waist Puffer Jacket",
+//         prodPrice: 1849.99,
+//         productSKU: 100240,
+//         productCategory: "Women",
+// 		productSubCategory: "Activewear",
+//     },
+
+
+//     "Black Dress": {
+//         productName: "Black Dress",
+//         prodPrice: 3299.00,
+//         productSKU: 101340,
+//         productCategory: "Women",
+// 		productSubCategory: "Fine Apparel"
+//     },
+//     "Red Dress": {
+//         productName: "Red Dress",
+//         prodPrice: 4720.00,
+//         productSKU: 101341,
+//         productCategory: "Women",
+// 		productSubCategory: "Fine Apparel"
+//     },
+//     "Sequin Dress": {
+//         productName: "Sequin Dress",
+//         prodPrice: 4250.00,
+//         productSKU: 101333,
+//         productCategory: "Women",
+// 		productSubCategory: "Fine Apparel"
+//     },
+//     "Silk Dress": {
+//         productName: "Silk Dress",
+//         prodPrice: 2449.99,
+//         productSKU: 101344,
+//         productCategory: "Women",
+// 		productSubCategory: "Fine Apparel"
+//     }
+
+// }
